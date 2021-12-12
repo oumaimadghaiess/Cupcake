bf709bc9c64d2c961bb399a457ddcd277098a328
oumaima dghaiess <oumaimadghaiess@gmail.com>
2021-12-12 13:24:25 +0100

    Initial commit

---
+++ app/src/main/java/com/oumaima/cupcake/FlavorFragment.kt
@@ -0,0 +1,63 @@
+package com.oumaima.cupcake
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.oumaima.cupcake.databinding.FragmentFlavorBinding
+import com.oumaima.cupcake.model.OrderViewModel
+
+/**
+ * [FlavorFragment] allows a user to choose a cupcake flavor for the order.
+ */
+class FlavorFragment : Fragment() {
+    private val sharedViewModel: OrderViewModel by activityViewModels()
+
+    // Binding object instance corresponding to the fragment_flavor.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var binding: FragmentFlavorBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val fragmentBinding = FragmentFlavorBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
+            flavorFragment = this@FlavorFragment
+        }
+    }
+
+    /**
+     * Navigate to the next screen to choose pickup date.
+     */
+    fun goToNextScreen() {
+        findNavController().navigate(R.id.action_flavorFragment_to_pickupFragment)
+    }
+
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_flavorFragment_to_startFragment)
+    }
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
---
+++ app/src/main/java/com/oumaima/cupcake/StartFragment.kt
@@ -0,0 +1,57 @@
+package com.oumaima.cupcake
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.oumaima.cupcake.databinding.FragmentStartBinding
+import com.oumaima.cupcake.model.OrderViewModel
+
+/**
+ * This is the first screen of the Cupcake app. The user can choose how many cupcakes to order.
+ */
+class StartFragment : Fragment() {
+    private val sharedViewModel: OrderViewModel by activityViewModels()
+
+    // Binding object instance corresponding to the fragment_start.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var binding: FragmentStartBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.startFragment = this
+    }
+
+    /**
+     * Start an order with the desired quantity of cupcakes and navigate to the next screen.
+     */
+     fun orderCupcake(quantity: Int) {
+        sharedViewModel.setQuantity(quantity)
+        if (sharedViewModel.hasNoFlavorSet()) {
+            sharedViewModel.setFlavor(getString(R.string.vanilla))
+        }
+        findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
+    }
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
